2cd84680d7d6a5ad470ceea91dc130885ab86923
saita <saita@kct.ac.jp>
2017-08-25 10:16:56 +0900

    Initial Commit

---
+++ Client/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Client
+//
+//  Created by SaitaSatoko on 2017/08/25.
+//  Copyright © 2017年 SaitaSatoko. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
